9145505fc92677230130a02dcb618ef54e2e0252
jkiedrowska85 <jkiedrowska85@gmail.com>
2022-08-19 16:22:39 +0200

    Zmiana switch case. Nie wiem jak zrobić, żeby szczytało mi wszystko z jednej lini?

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -5,30 +5,65 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            double number1, number2;
-            string operation;
- 
-            //asia ma psa :) 
-            if ( operation == "dodaj" )
             {
-                Console.WriteLine("Wynik: " + (number1 + number2));
-            }
-            else
-            if ( operation == "odejmij")
-            {
-                Console.WriteLine("Wynik: " + (number1 - number2));
-            }
-            else
-            if (operation == "pomnóż")
-            {
-                Console.WriteLine("Wynik: " + (number1 * number2));
-            }
-            else
-            if (operation == "podziel")
-            {
-                Console.WriteLine("Wynik: " + (number1 / number2));
-            }
-        }
+                string operation = Console.ReadLine();
+                double number1;
+                double number2;
+                bool resParse1, resParse2;
+                resParse1 = double.TryParse(Console.ReadLine(), out number1);
+                resParse2 = double.TryParse(Console.ReadLine(), out number2);
 
+                switch (operation)
+                {
+                    case "dodaj":
+                        {
+                            Console.WriteLine("Wynik: " + (number1 + number2));
+                            break;
+                        }
+                    case "odejmij":
+                        {
+                            Console.WriteLine("Wynik: " + (number1 - number2));
+                            break;
+                        }
+                    case "mnóż":
+                        {
+                            Console.WriteLine("Wynik: " + (number1 * number2));
+                            break;
+                        }
+                    case "dziel":
+                        {
+                            Console.WriteLine("Wynik: " + (number1 / number2));
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Podałeś złą operacje");
+                            break;
+                        }
+                }
+
+                //}
+                //if ( operation == "dodaj" )
+                //{
+                //    Console.WriteLine("Wynik: " + (number1 + number2));
+                //}
+                //else
+                //if ( operation == "odejmij")
+                //{
+                //    Console.WriteLine("Wynik: " + (number1 - number2));
+                //}
+                //else
+                //if (operation == "pomnóż")
+                //{
+                //    Console.WriteLine("Wynik: " + (number1 * number2));
+                //}
+                //else
+                //if (operation == "podziel")
+                //{
+                //    Console.WriteLine("Wynik: " + (number1 / number2));
+                //}
+            }
+
+        }
     }
 }
