2b9e99e2d49325b71da79f11939898c64c67f7ee
jkiedrowska85 <jkiedrowska85@gmail.com>
2022-08-19 13:40:07 +0200

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number1, number2;
+            string operation;
+ 
+            if ( operation == "dodaj" )
+            {
+                Console.WriteLine("Wynik: " + (number1 + number2));
+            }
+            else
+            if ( operation == "odejmij")
+            {
+                Console.WriteLine("Wynik: " + (number1 - number2));
+            }
+            else
+            if (operation == "pomnóż")
+            {
+                Console.WriteLine("Wynik: " + (number1 * number2));
+            }
+            else
+            if (operation == "podziel")
+            {
+                Console.WriteLine("Wynik: " + (number1 / number2));
+            }
+        }
+
+    }
+}
